a77ce460d3c568ecd4ac43cd21de4e37eab09afb
seojeil <70929964+Seojeil@users.noreply.github.com>
2025-05-26 13:27:46 +0900

    [Gold V] Title: 회의실 배정, Time: 248 ms, Memory: 54216 KB -BaekjoonHub

--- Python/백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ Python/백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,9 +1,10 @@
-N = int(input())
+import sys
+input = sys.stdin.readline
 
 meetings = []
 
-for _ in range(N):
-    meetings.append(tuple(map(int, input().split())))
+N = int(input())
+meetings = [tuple(map(int, input().split())) for _ in range(N)]
 
 meetings.sort(key=lambda x: (x[1], x[0]))
 end_time = 0
