269ea4e5c3024e1dc77e8644d4a6d0ffb23f0255
seojeil <70929964+Seojeil@users.noreply.github.com>
2025-07-30 10:14:28 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ Python/백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -5,27 +5,22 @@ input = sys.stdin.readline
 weight = int(input())
 
 def sugar(weight):
-    if weight == 4 or weight == 7:
+    dp = [float('inf')] * (weight + 1)
+
+    dp[0] = 0
+
+    for i in range(weight + 1):
+        if i >= 3:
+            if dp[i-3] != -1:
+                dp[i] = min(dp[i], dp[i-3]+1)
+
+        if i >= 5:
+            if dp[i-5] != -1:
+                dp[i] = min(dp[i], dp[i-5]+1)
+
+    if dp[weight] == float('inf'):
         return -1
-    
-    if weight == 3:
-        return 1
-    
-    if weight % 5 == 0:
-        return weight // 5
-
-    dp = {i: [] for i in range(weight // 5 + 2)}
-
-    dp[1] = [3, 5]
-    
-
-    for i in range(2, weight//5 + 2):
-        for j in dp[i-1]:
-            if j+3 == weight:
-                return i
-            dp[i].append(j+3)
-        dp[i].append(max(dp[i-1])+5)
-    
-    return len(dp)
+    else:
+        return dp[weight]
 
 print(sugar(weight))
