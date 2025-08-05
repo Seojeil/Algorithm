b958031056768a2aa720902ff0bc91e145e77362
seojeil <70929964+Seojeil@users.noreply.github.com>
2025-08-06 07:14:51 +0900

    [Bronze III] Title: TGN, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+for _ in range(N):
+    r, e, c = map(int, input().split())
+
+    if r < e - c:
+        print("advertise")
+    elif r > e - c:
+        print("do not advertise")
+    else:
+        print("does not matter")
