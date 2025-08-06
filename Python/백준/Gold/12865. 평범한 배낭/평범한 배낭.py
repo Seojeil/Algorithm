e4a2433ded4d5aed4c3062c49ee2fe81504b81d5
seojeil <70929964+Seojeil@users.noreply.github.com>
2025-08-06 16:22:30 +0900

    [Gold V] Title: 평범한 배낭, Time: 1724 ms, Memory: 228328 KB -BaekjoonHub

--- Python/백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ Python/백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,25 +2,24 @@ import sys
 
 input = sys.stdin.readline
 
-N, K = map(int, input().split())
+def solve_knapsack():
+    N, K = map(int, input().split())
+    items = []
 
-items = []
+    for _ in range(N):
+        items.append(tuple(map(int, input().split())))
 
-for _ in range(N):
-    items.append(tuple(map(int, input().split())))
+    dp = [[0] * (K+1) for _ in range(N+1)]
 
-def bag(items, n, k):
-    dp = [[0] * (k + 1) for _ in range(n + 1)]
-
-    for i in range(1, n+1):
+    for i in range(1, N+1):
         w, v = items[i-1]
-        
-        for j in range(1, k+1):
-            if j < w:
-                dp[i][j] = dp[i-1][j]
-            else:
+        for j in range(1, K+1):
+            if w <= j:
                 dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
-    
-    return dp[n][k]
+            else:
+                dp[i][j] = dp[i-1][j]
 
-print(bag(items, N, K))
+    print(dp[N][K])
+
+# 실행
+solve_knapsack()
