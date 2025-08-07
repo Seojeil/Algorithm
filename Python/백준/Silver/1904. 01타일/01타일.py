e744d10e893c8557b541f84f91f307f5d8597f40
seojeil <70929964+Seojeil@users.noreply.github.com>
2025-08-07 12:18:36 +0900

    [Silver III] Title: 01타일, Time: 208 ms, Memory: 71968 KB -BaekjoonHub

--- Python/백준/Silver/1904. 01타일/01타일.py
+++ Python/백준/Silver/1904. 01타일/01타일.py
@@ -3,18 +3,15 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-
-def tile(n):
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 2
-
-    dp = [0] * (n+1)
     
+def tile(n):
+    if n == 1 or n == 2:
+        return n
+    dp = [0] * (n+1)
+
     dp[1] = 1
     dp[2] = 2
-
+    
     for i in range(3, n+1):
         dp[i] = (dp[i-2] + dp[i-1]) % 15746
 
