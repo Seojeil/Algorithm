12b29c5e6b4d9bd2ee4c3e4591c84bf7fc476214
seojeil <70929964+Seojeil@users.noreply.github.com>
2025-11-17 18:03:15 +0900

    [Bronze III] Title: 꿍의 우주여행, Time: 148 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/9501. 꿍의 우주여행/꿍의 우주여행.py
@@ -0,0 +1,14 @@
+T = int(input())
+
+for _ in range(T):
+    N, D = map(int, input().split())
+
+    n = 0
+
+    for _ in range(N):
+        v, f, c = map(int, input().split())
+
+        if v * f / c >= D:
+            n += 1
+
+    print(n)
